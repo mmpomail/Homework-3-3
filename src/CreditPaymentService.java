8cf5305a243e9f15a1153c69b802a67ed069af17
MailMan <ainddal@gmail.com>
2024-02-24 13:46:53 +0300

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,21 @@
+public class CreditPaymentService {
+    public int calculate (int month, int sum, float per){
+
+        float result;
+        float del;
+        float det;
+        float step;
+        float c;
+        float coff; // коэффициент аннуитетного платежа, рассчитываемый по формуле
+
+        c = 1+per;
+        step = (float)Math.pow(c, month);
+        del = per*step;
+        det = step - 1;
+        coff = del/det;
+        result = coff*sum; // умножение коэффициента на сумму платежа, для выяснения ежемесячного платежа
+        return (int) result; // итоговый результат в целочисленном значении
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService serv = new CreditPaymentService();
+        int month = 12; // период платежа в месяцах
+        float per = 0.00789F; // ежемесячные проценты по платежу
+        int sum = 1_000_000; // общая сумма платежа в рублях
+
+        System.out.println();
+        System.out.println("Ваш ежемесячный платёж: " + serv.calculate (12, sum,per));
+        System.out.println();
+        System.out.println("Ваш ежемесячный платёж: " + serv.calculate (24, sum, per));
+        System.out.println();
+        System.out.println("Ваш ежемесячный платёж: " + serv.calculate (36, sum, per));
+
+    }
+}
